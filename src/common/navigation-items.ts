daff9ce768f2ede339a0603a3c1d205de5c037f3
Joseph Brian Eugenio <jbeugenio34@gmail.com>
2024-05-05 22:30:40 +0800

    Added navigation files

--- src/common/navigation-items.ts
+++ src/common/navigation-items.ts
@@ -1,5 +1,10 @@
 import HomeImage from '@/assets/logo-red.png'
 import Orders from '@/assets/chicken.png'
+import { useRoute } from "vue-router";
+import { cartOutline, timer, location, heart, fastFood, notifications, storefront, helpCircle, cart, person } from "ionicons/icons";
+
+// const route = useRoute();
+
 
 
 export type RouteType = {
@@ -10,7 +15,9 @@ export type RouteType = {
     label: string
     separator?: boolean
     route?: RouteType
-    img: string | any
+    img?: string | any
+    icon?: string
+    isActive?: boolean
   }
 
   export const MenuItems: Array <NavigationItems> = [
@@ -22,49 +29,62 @@ export type RouteType = {
     },
     {
         label: 'Order Now!',
-        img: Orders,
+        icon: fastFood,
         separator: false,
-        route: { name: 'home.page' },
+        route: { name: 'food.page' },
     },
-    // {
-    //     label: 'Home',
-    //     separator: false,
-    //     // route: 
-    // },
-    // {
-    //     label: 'Home',
-    //     separator: false,
-    //     // route: 
-    // },
-    // {
-    //     label: 'Home',
-    //     separator: false,
-    //     // route: 
-    // },
-    // {
-    //     label: 'Home',
-    //     separator: false,
-    //     // route: 
-    // },
-    // {
-    //     label: 'Home',
-    //     separator: false,
-    //     // route: 
-    // },
-    // {
-    //     label: 'Home',
-    //     separator: false,
-    //     // route: 
-    // },
-    // {
-    //     label: 'Home',
-    //     separator: false,
-    //     // route: 
-    // },
-    // {
-    //     label: 'Home',
-    //     separator: false,
-    //     // route: 
-    // },
+    {
+        label: 'Notiifications',
+        separator: false,
+        icon: notifications
+        // route: 
+    },
+    {
+        label: 'Store Locator',
+        separator: false,
+        icon: storefront
+        // route: 
+    },
+    {
+        label: `FAQ's`,
+        separator: false,
+        icon:helpCircle
+        // route: 
+    },
+    {
+        label: 'My Orders',
+        separator: false,
+        icon:cart
+        // route: 
+    },
+    {
+        label: 'My Account',
+        separator: false,
+        icon: person
+        // route: 
+    },
+    {
+        label: 'My Favorites',
+        separator: false,
+        icon: heart
+        // route: 
+    },
+    {
+        label: 'Order Tracker',
+        separator: false,
+        icon: location
+        // route: 
+    },
+    {
+        label: 'Order History',
+        separator: false,
+        icon: timer
+        // route: 
+    },
 
   ]
+
+  export function calculateIsActive(routeName: string): boolean {
+    const route = useRoute();
+    return route.name === routeName;
+}
