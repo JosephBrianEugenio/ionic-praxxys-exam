eec05910c6493e7343faffe41cd6472ddcb00a0d
Joseph Brian Eugenio <jbeugenio34@gmail.com>
2024-05-05 22:30:22 +0800

    Added staticRating component

---
+++ src/common/Components/StaticRating.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import {
+  IonIcon,
+} from "@ionic/vue";
+import { starHalf, star } from "ionicons/icons";
+</script>
+
+<template>
+  <div>
+    <IonIcon :icon="star" color="warning"></IonIcon>
+    <IonIcon :icon="star" color="warning"></IonIcon>
+    <IonIcon :icon="star" color="warning"></IonIcon>
+    <IonIcon :icon="star" color="warning"></IonIcon>
+    <IonIcon :icon="starHalf" color="warning"></IonIcon>
+  </div>
+</template>
+
+<style scoped></style>
